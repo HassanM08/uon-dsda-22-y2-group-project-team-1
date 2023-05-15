128351e1a5f920d1ae057cc2d3ab477adbd17674
bruuud <94838738+bruuud@users.noreply.github.com>
2023-05-15 15:13:19 +0100

    Web scraping - Productionise Web Scraping (#8)
    * folder structure change
    
    * web scraping module
    
    * SPIKE updates
    
    * hard coded values
    
    * isort
    
    * spikes and testing
    
    * rightmove sold houses web scraping
    
    * docstrings
    
    * over 88 char limit
    
    * formatting
    
    ---------
    
    Co-authored-by: Brud1 <bradjonjepson@yahoo.co.uk>

--- backend/__init__.py
+++
@@ -1,3 +0,0 @@
-YOUR_MOVE_URL = "https://www.your-move.co.uk"
-BRANCHES_URL = f"{YOUR_MOVE_URL}/branches"
-ESTATE_AGENTS_URL = f"{YOUR_MOVE_URL}/estate-agent"
---
+++ service/__init__.py
@@ -0,0 +1,16 @@
+YOUR_MOVE_URL = "https://www.your-move.co.uk"
+BRANCHES_URL = f"{YOUR_MOVE_URL}/branches"
+ESTATE_AGENTS_URL = f"{YOUR_MOVE_URL}/estate-agent"
+
+PROPERTY_TYPES = [
+    "house",
+    "property",
+    "detached",
+    "semi detached",
+    "link detached",
+    "end terrace",
+    "mid terrace",
+    "flat",
+    "bungalow",
+    "land/plot",
+]
